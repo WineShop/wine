7c0026fed20bb691667e965d011ec6253975f3a0
aa791845283 <kevin.liu@yunzhihui.com>
2015-10-13 11:34:11 +0800

    添加团购功能

--- Application/Home/Controller/ArticleController.class.php
+++ Application/Home/Controller/ArticleController.class.php
@@ -201,58 +201,70 @@ class ArticleController extends HomeController {
  public function detail($id = 0, $p = 1){
 		/* 热词调用*/
         // unset($_SESSION['cart']);	
-		$hotsearch=R("Index/getHotsearch");
-		$this->assign('hotsearch',$hotsearch);
-		/* 购物车调用*/
-		$cart=R("Shopcart/usercart");
-		$this->assign('usercart',$cart);
-		if(!session('user_auth')){
-			$usercart=$_SESSION['cart'];
-		$this->assign('usercart',$usercart);
-		}
-		/* 底部分类调用*/
-		$menulist=R('Service/AllMenu');
+         $hotsearch = C('HOT_SEARCH');
+         $this->assign('hotsearch',$hotsearch);
+         /** 购物车调用**/
+         $cart=R("Shopcart/usercart");
+         $this->assign('usercart',$cart);
+         if(!session('user_auth')){
+             $usercart=$_SESSION['cart'];
+             $this->assign('usercart',$usercart);
+         }
+
+        /** 底部分类调用**/
+         if(S(C('HOME_FOOT_MENU')))
+         {
+             $menulist = S(C('HOME_FOOT_MENU'));
+         }else{
+             $menulist = R('Service/AllMenu');
+             S(C('HOME_FOOT_MENU'),$menulist,3600*24*30);
+         }
 		$this->assign('footermenu',$menulist);
+
 		/* 左侧分类列表*/
-		$mlist=R('Index/menulist');
+		$mlist=R('Menu/menulist');
 		$this->assign('categoryq', $mlist);
+
 		/* 浏览量排行前7个商品*/
 		$view=M('Document')->where("display=1 and status=1")->order("view desc")->select();
 		$this->assign('viewlist', $view);
+
 		/* 标识正确性检测 */
 		if(!($id && is_numeric($id))){
-		$this->error('文档ID错误！');
+		    $this->error('文档ID错误！');
 		}	
+
 		/* 获取详细信息 */
 		$Document = D('Document');
 		$info = $Document->detail($id);
 		if(!$info){
-		$this->error($Document->getError());
-		} 
+		    $this->error($Document->getError());
+		}
+
 		/* 分类信息 */
 		$category = $this->category($info['category_id']);
 		/* 获取模板 */
 		if(!empty($info['template'])){//已定制模板
-		$tmpl = $info['template'];
-		} 
-		elseif (!empty($category['template_detail'])){ 
+		    $tmpl = $info['template'];
+		}elseif (!empty($category['template_detail'])){
 			//分类已定制模板
-		$tmpl = $category['template_detail'];
+		    $tmpl = $category['template_detail'];
 		} else { 
 			//使用默认模板
-		$tmpl = 'Article/'. get_document_model($info['model_id'],'name') .'/detail';
+		    $tmpl = 'Article/'. get_document_model($info['model_id'],'name') .'/detail';
 		}
-		
+
 		/* 更新浏览数 */
 		$map = array('id' => $id);
 		$Document->where($map)->setInc('view');
 		/*内容页统计代码实现，tag=3*/
 		if(1==C('IP_TONGJI')){
-		$record=IpLookup("",3,$id);
+		    $record=IpLookup("",3,$id);
 		}
-		/*获取商品所有评论*/
+
+		/**获取商品所有评论**/
 		$comment = M('comment');	
-		$count = $comment->where("status='1' and goodid='$id'")->count(); //计算记录数
+		$count   = $comment->where("status='1' and goodid='$id'")->count(); //计算记录数
         $this->assign('count', $count);
 		$limitRows = 5; // 设置每页记录数
 		$p = new \Think\AjaxPage($count, $limitRows,"comment"); //第三个参数是你需要调用换页的ajax函数名
@@ -262,55 +274,57 @@ class ArticleController extends HomeController {
 		$this->assign('list',$data);
 		$this->assign('page',$page);
 
-       /*获取商品所有好评*/
-	$comment = M('comment');	
-		$countbetter = $comment->where("status='1' and goodid='$id' and score='3'")->count(); //计算记录数
+       /**获取商品所有好评**/
+	    $comment        = M('comment');
+		$countbetter    = $comment->where("status='1' and goodid='$id' and score='3'")->count(); //计算记录数
         $this->assign('countbetter',$countbetter);
 		$limitRowsbetter = 5; // 设置每页记录数
-		$pbetter = new \Think\AjaxPage($countbetter, $limitRowsbetter,"commentgood"); //第三个参数是你需要调用换页的ajax函数名
-		$limitbetter = $pbetter->firstRow . "," . $pbetter->listRows;
-		$listbetter = $comment->where("status='1' and goodid='$id' and score='3'")->order('id desc')->limit($limitbetter)->select(); // 查询数据
-		$pagebetter = $pbetter->show(); // 产生分页信息，AJAX的连接在此处生成
+		$pbetter        = new \Think\AjaxPage($countbetter, $limitRowsbetter,"commentgood"); //第三个参数是你需要调用换页的ajax函数名
+		$limitbetter    = $pbetter->firstRow . "," . $pbetter->listRows;
+		$listbetter     = $comment->where("status='1' and goodid='$id' and score='3'")->order('id desc')->limit($limitbetter)->select(); // 查询数据
+		$pagebetter     = $pbetter->show(); // 产生分页信息，AJAX的连接在此处生成
 		$this->assign('listbetter',$listbetter);
 		$this->assign('pagebetter',$pagebetter);
 
       /*获取商品所有中评*/
-		$comment = M('comment');	
-		$countmiddle= $comment->where("status='1' and goodid='$id' and score='2'")->count(); //计算记录数
+		$comment        = M('comment');
+		$countmiddle    = $comment->where("status='1' and goodid='$id' and score='2'")->count(); //计算记录数
         $this->assign('countmiddle ', $countmiddle);
-		$limitRows = 5; // 设置每页记录数
-		$pmiddle = new \Think\AjaxPage($countmiddle, $limitRows,"commentmiddle"); //第三个参数是你需要调用换页的ajax函数名
-		$limitmiddle = $pmiddle->firstRow . "," . $pmiddle->listRows;
-		$datamiddle = $comment->where("status='1' and goodid='$id' and score='2'")->order('id desc')->limit($limitmiddle)->select(); // 查询数据
-		$pagemiddle = $pmiddle->show(); // 产生分页信息，AJAX的连接在此处生成
+		$limitRows      = 5; // 设置每页记录数
+		$pmiddle        = new \Think\AjaxPage($countmiddle, $limitRows,"commentmiddle"); //第三个参数是你需要调用换页的ajax函数名
+		$limitmiddle    = $pmiddle->firstRow . "," . $pmiddle->listRows;
+		$datamiddle     = $comment->where("status='1' and goodid='$id' and score='2'")->order('id desc')->limit($limitmiddle)->select(); // 查询数据
+		$pagemiddle     = $pmiddle->show(); // 产生分页信息，AJAX的连接在此处生成
 		$this->assign('listmiddle',$datamiddle);
 		$this->assign('pagemiddle',$pagemiddle);
           
 		 /*获取商品所有差评*/
-		$comment = M('comment');	
-		$countworse= $comment->where("status='1' and goodid='$id' and score='1'")->count(); //计算记录数
+		$comment        = M('comment');
+		$countworse     = $comment->where("status='1' and goodid='$id' and score='1'")->count(); //计算记录数
         $this->assign('countworse ', $countworse);
-		$limitRows = 5; // 设置每页记录数
-		$pworse = new \Think\AjaxPage($countworse, $limitRows,"commentworse"); 
+		$limitRows      = 5; // 设置每页记录数
+		$pworse         = new \Think\AjaxPage($countworse, $limitRows,"commentworse");
 		//第三个参数是你需要调用换页的ajax函数名
-		$limitworse = $pworse->firstRow . "," . $pworse->listRows;
-		$dataworse = $comment->where("status='1' and goodid='$id' and score='1'")->order('id desc')->limit($limitworse)->select(); // 查询数据
-		$pageworse = $pworse->show(); // 产生分页信息，AJAX的连接在此处生成
+		$limitworse     = $pworse->firstRow . "," . $pworse->listRows;
+		$dataworse      = $comment->where("status='1' and goodid='$id' and score='1'")->order('id desc')->limit($limitworse)->select(); // 查询数据
+		$pageworse      = $pworse->show(); // 产生分页信息，AJAX的连接在此处生成
 		$this->assign('listworse',$dataworse);
 		$this->assign('pageworse',$pageworse);
     	/* 好评率 */
-		$rate=($countbetter/$count)*100;
+		$rate           = ($countbetter/$count)*100;
         $this->assign('rate', $rate);
-         /* 咨询管理 */
-		$message=M("message");
-		$reply=M("reply");
-		$countmessage=$message->where(" goodid='$id'")->count();
-		$Pagequestion=new \Think\AjaxPage($countmessage,5,"quest");	
-		$limitquestion = $Pagequestion->firstRow . "," . $Pagequestion->listRows;
-		$showquestion= $Pagequestion->show();
-		$listquestion=$message->where("goodid='$id'")->order('id desc')->limit($limitquestion)->select();
-		foreach($listquestion as $n=> $val){
-		$listquestion[$n]['id']=$reply->where('messageid=\''.$val['id'].'\'')->select();
+
+
+         /** 咨询管理 **/
+		$message        = M("message");
+		$reply          = M("reply");
+		$countmessage   = $message->where(" goodid='$id'")->count();
+		$Pagequestion   =   new \Think\AjaxPage($countmessage,5,"quest");
+		$limitquestion  = $Pagequestion->firstRow . "," . $Pagequestion->listRows;
+		$showquestion   = $Pagequestion->show();
+		$listquestion   = $message->where("goodid='$id'")->order('id desc')->limit($limitquestion)->select();
+		foreach($listquestion as $n => $val){
+		    $listquestion[$n]['id'] = $reply->where('messageid=\''.$val['id'].'\'')->select();
 		}
 		$this->assign('listquestion',$listquestion);// 赋值数据集
 		$this->assign('pagequestion',$showquestion);//
@@ -319,7 +333,7 @@ class ArticleController extends HomeController {
 		$this->assign('info', $info);
 		$this->meta_title = $info["title"];
 		$this->display($tmpl);
-		}
+}
 /* ajax评论-所有评论 */
  public function comment(){	
 	 if($_POST["goodid"])
