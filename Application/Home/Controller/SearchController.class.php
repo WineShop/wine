3e98b3cc0d56478df6ca8145f1727092645b13f6
kevin.liu <kevin.liu@yunzhihui.com>
2015-11-17 01:46:01 +0800

    搜索内容

--- Application/Home/Controller/SearchController.class.php
+++ Application/Home/Controller/SearchController.class.php
@@ -4,8 +4,6 @@
 // +----------------------------------------------------------------------
 // | Copyright (c) 2013 http://www.onethink.cn All rights reserved.
 // +----------------------------------------------------------------------
-// | Author: 麦当苗儿 <zuojiazi@vip.qq.com> <http://www.zjzit.cn>
-// +----------------------------------------------------------------------
 
 namespace Home\Controller;
 
@@ -17,69 +15,52 @@ class SearchController extends HomeController {
 
     /* 文档模型频道页 */
 	public function index(){
-    $keyword= I('words');//获取分类的英文名称
-  $key=I('get.order');
-		 $sort=I('get.sort');  
+        $keyword = I('words');//获取分类的英文名称
+        $key     = I('get.order');
+        $sort    = I('get.sort');
        if(isset($key)){
-		  
-		if($key=="1"){ $listsort="view"." ".$sort;}  
-		if($key=="2"){ $listsort="id"." ".$sort;} 
-		if($key=="3"){  $listsort="price"." ".$sort;} 
-		if($key=="4"){  $listsort="sale"." ".$sort;}  	
-		   } 
-	if(empty($key)){$key="1";
-	$see="asc";
-			$order="view";
-			$sort="asc";
-		    $listsort=$order." ".$sort;			
-			}
-		
-    if($sort=="asc"){$see="desc";}
-      if($sort=="desc"){$see="asc";}
-      $this->assign('see',$see);
-      $this->assign('order',$key);
-	  $this->assign('value',$sort);
+            if($key=="1"){ $listsort="view"." ".$sort;}
+            if($key=="2"){ $listsort="id"." ".$sort;}
+            if($key=="3"){  $listsort="price"." ".$sort;}
+            if($key=="4"){  $listsort="sale"." ".$sort;}
+       }
+        if(empty($key)){$key="1";
+            $see="asc";
+            $order="view";
+            $sort="asc";
+            $listsort=$order." ".$sort;
+        }
 
-	$map['title|name|description']  = array('like','%'.$keyword.'%');
-     $count=M('Document')->where($map)->count();
-     $Page= new \Think\Page($count,12);
-	  $Page->setConfig('prev','上一页');
-      $Page->setConfig('next','下一页');
-      $Page->setConfig('first','第一页');
-      $Page->setConfig('last','尾页');
-      $Page->setConfig('theme','%FIRST% %UP_PAGE% %LINK_PAGE% %DOWN_PAGE% %END% %HEADER%');
-      $show= $Page->show();    
-      $list=M('Document')->where($map)->order($listsort)->limit($Page->firstRow.','.$Page->listRows)->select();
-     $this->assign('list',$list);// 赋值数据集
-$this->assign('page',$show);
-/*销量排行*/
-		$sales=R('Article/ranks');
-		$this->assign('sales', $sales);
-		/*最近访问*/
-		$recent=R('Article/view_recent');
-		$this->assign('recent', $recent);
+         if($sort=="asc"){$see="desc";}
+         if($sort=="desc"){$see="asc";}
+          $this->assign('see',$see);
+          $this->assign('order',$key);
+          $this->assign('value',$sort);
 
-     $this->assign('searchlist', $list);
-	  $this->assign('keyword', $keyword);
-	 /* 左侧菜单 */
-	 $menu=R('index/menulist');
-	 $this->assign('categoryq', $menu);
-    /* 购物车调用*/
-     $cart=R("shopcart/usercart");
-    $this->assign('usercart',$cart);
-      if(!session('user_auth'))
-		{ 
-		 $usercart=$_SESSION['cart'];
-       $this->assign('usercart',$usercart); 
-	    }
-		   /* 底部分类调用*/
-   $menulist=R('Service/AllMenu');
-   $this->assign('footermenu',$menulist);
-  /* 热词调用*/
-    $hotsearch=R("Index/getHotsearch");
-    $this->assign('hotsearch',$hotsearch);
-	 $this->meta_title = $keyword.'的搜索结果';
-		$this->display();
-	}
+          $map['title|name|description']  = array('like','%'.$keyword.'%');
+          $count=M('Document')->where($map)->count();
+          $Page= new \Think\Page($count,12);
+          $Page->setConfig('prev','上一页');
+          $Page->setConfig('next','下一页');
+          $Page->setConfig('first','第一页');
+          $Page->setConfig('last','尾页');
+          $Page->setConfig('theme','%FIRST% %UP_PAGE% %LINK_PAGE% %DOWN_PAGE% %END% %HEADER%');
+          $show= $Page->show();
+
+          $field     = "id,title,price,tuan_price,qg_price,ms_price,brand,fengmian";
+          $list=M('Document')->where($map)->field($field)->order($listsort)->limit($Page->firstRow.','.$Page->listRows)->select();
+          $this->assign('list',$list);// 赋值数据集
+          $this->assign('page',$show);
+
+
+          $this->assign('searchlist', $list);
+          $this->assign('keyword', $keyword);
+
+        /* 热词调用*/
+        $hotsearch = C('HOT_SEARCH');
+        $this->assign('hotsearch',$hotsearch);
+        $this->meta_title = $keyword.'的搜索结果';
+        $this->display();
+    }
 
 }
